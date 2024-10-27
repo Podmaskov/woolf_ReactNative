1bd3dee86fb8dbd2eb8844a92b34e0dc787f02bd
Artem <podmaskov@gmail.com>
2024-10-27 16:17:28 +0100

    hw-2 (#2)

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,8 +1,30 @@
-import React from "react";
-import { TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
-export const Button = ({ children, ...TouchableOpacityProps }) => {
+export default function Btn({ text, onPress, disabled, style, styleTitle }) {
   return (
-    <TouchableOpacity {...TouchableOpacityProps}>{children}</TouchableOpacity>
+    <TouchableOpacity
+      activeOpacity={0.8}
+      style={{ ...styles.button, ...style }}
+      onPress={onPress}
+      disabled={disabled}
+    >
+      <Text style={{ ...styles.title, ...styleTitle }}>{text}</Text>
+    </TouchableOpacity>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  button: {
+    height: 50,
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+    marginTop: 43,
+    alignItems: "center",
+    padding: 16,
+  },
+  title: {
+    fontFamily: "Roboto-Regular",
+    color: "#FFFFFF",
+    fontSize: 16,
+  },
+});
